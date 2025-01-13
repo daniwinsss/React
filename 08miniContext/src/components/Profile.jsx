c9a51280c348cd412517c1baa861a1e8ecb2bf68
daniwinsss <dankdani06@gmail.com>
2025-01-13 23:38:12 +0530

    commit

---
+++ 08miniContext/src/components/Profile.jsx
@@ -0,0 +1,8 @@
+import React, { useState,useContext } from "react";
+import UserContext from "../context/UserContext";
+function Profile(){
+    const {user} = useContext(UserContext);
+    if(!user) return <div>please login</div>
+    return <div>Welcome {user.username}</div>
+}
+export default Profile;
