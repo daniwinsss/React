a64b9b7674750018b6d9747a1c0c243cba7e824d
daniwinsss <dankdani06@gmail.com>
2025-02-06 00:19:21 +0530

    commit

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -1,5 +1,54 @@
 import conf from '../conf/conf.js'
 import {Client,Account,Id} from  'appwrite'
-export class AuthService {}
+export class AuthService {
+    client = new Client();
+    account;
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId)
+        this.account = new Account(this.client)
+    }
+    async createAccount({email,password,name}){
+        try{
+            const userAccount = await this.account.create(Id.unique(),email,password,name);
+            if(userAccount){
+                //another method
+                return this.login({email,password});
+            }
+            else{
+                return userAccount;
+            }
+        }
+        catch(error){
+            throw error;
+        }
+    }
+    async login({email,password}){
+        try{
+            return await this.account.createEmailSession(email,password);
+        }
+        catch(error){
+            throw error;
+        }
+    }
+    async getCurrentUser(){
+        try{
+            return await this.account.get();
+        }
+        catch(error){
+            throw error;
+        }
+        return null;
+    }
+    async logout(){
+        try{
+           return await this.account.deleteSessions();
+        }
+        catch(error){
+            throw error;
+        }
+    }
+}
 const AuthService = new AuthService();
 export default AuthService
