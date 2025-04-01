074834043a7e331cac4cfa8c8394cef1a1f94502
daniwinsss <dankdani06@gmail.com>
2025-04-01 16:37:38 +0530

    commit

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,4 +3,5 @@ import Footer from './Footer/Footer.jsx';
 import Container from './Container/Container.jsx'; 
 import Logo from './Logo.jsx'; 
 import LogoutBtn from './Header/LogoutBtn.jsx';
-export { Header, Footer, Container , Logo , LogoutBtn};
+import RTE from './RTE.jsx';
+export { Header, Footer, Container , Logo , LogoutBtn,RTE};
